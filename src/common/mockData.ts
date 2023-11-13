85c02218b33059cb799c0d2088ddedd49948490c
Arief Nugroho <arief@nftseedlab.com>
2023-11-13 11:54:56 +0700

    add mockup data

---
+++ src/common/mockData.ts
@@ -0,0 +1,109 @@
+export const titleAspekPenilaian = [
+  "Aspek penilaian 1",
+  "Aspek penilaian 2",
+  "Aspek penilaian 3",
+  "Aspek penilaian 4",
+]
+
+export const mockTablePenilaianMahasiswa = [
+  {
+    label: "Mahasiswa 1",
+    name: "mahasiswa_1",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 2",
+    name: "mahasiswa_2",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 3",
+    name: "mahasiswa_3",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 4",
+    name: "mahasiswa_4",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 5",
+    name: "mahasiswa_5",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 6",
+    name: "mahasiswa_6",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 7",
+    name: "mahasiswa_7",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 8",
+    name: "mahasiswa_8",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 9",
+    name: "mahasiswa_9",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+  {
+    label: "Mahasiswa 10",
+    name: "mahasiswa_10",
+    ariaLabelScore: [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ],
+  },
+]
